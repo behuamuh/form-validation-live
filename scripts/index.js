f69c443a55d5020ee2fe3e5f69f008e0e09e2f52
Behuamuh <sven1980@list.ru>
2021-05-13 21:08:07 +0300

    feat: done

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
-enableValidation();
+const config = {
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__submit-button',
+  inputErrorClass: 'form__input_type_error',
+  errorActiveClass: 'form__input-error_active',
+};
+
+enableValidation(config);
+
+config.inputErrorClass = 'bad-class';
+
+// enableValidation({
+//   formSelector: '.form',
+//   inputSelector: '.form__input',
+//   submitButtonSelector: '.form__submit-button',
+//   inputErrorClass: 'form__input_type_error',
+//   errorActiveClass: 'form__input-error_active',
+// });
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,98 @@
-const enableValidation = () => {
+const hideInputError = (formElement, inputElement, config) => {
+  // hide error
+  // find error elemet
+  const { inputErrorClass, errorActiveClass } = config;
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorActiveClass);
+  errorElement.textContent = '';
+}
+
+const showInputError = (formElement, inputElement, config) => {
+  // show error
+  const { inputErrorClass, errorActiveClass } = config;
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = inputElement.validationMessage;
+  errorElement.classList.add(errorActiveClass);
+}
+
+const checkInputValidity = (formElement, inputElement, config) => {
+  // check input is valid
+  if (inputElement.validity.valid) {
+    hideInputError(formElement, inputElement, config);
+  } else {
+    showInputError(formElement, inputElement, config);
+  }
+  // if valid, hide error else show error
+}
+
+const hazInvalidInput = (inputList) => {
+  return inputList.some(inputElement => !inputElement.validity.valid);
+}
+
+const toggleButtonState = (buttonElement, inputList) => {
+  // if form valid enable button else disable
+  if (hazInvalidInput(inputList)) {
+    // disable
+    buttonElement.disabled = true;
+  } else {
+    // enable
+    buttonElement.disabled = false;
+  }
+}
+
+// const checkPasswordConfirm = (formElement) => {
+//   // find fields
+//   const passwordElement = formElement.querySelector('#new-password');
+//   const passwordConfirmElement = formElement.querySelector('#new-password-confirm');
+
+//   if (!passwordElement || !passwordConfirmElement) return;
+
+//   if (passwordElement.value === passwordConfirmElement.value) {
+//     // hide confirm error
+//     passwordConfirmElement.setCustomValidity('');
+//   } else {
+//     // show confirm error
+//     passwordConfirmElement.setCustomValidity('Пароли не совпадают!');
+//   }
+// }
+
+const setEventListeners = (formElement, config) => {
+  // prevent page reload on form submit
+  const { inputSelector, submitButtonSelector, ...restConfig } = config;
+
+  formElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+  });
   
+  // find all inputs
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+
+  // find submit button
+  const buttonElement = formElement.querySelector(submitButtonSelector);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      // check input is valid
+      // checkPasswordConfirm(formElement);
+      checkInputValidity(formElement, inputElement, restConfig);
+      toggleButtonState(buttonElement, inputList);
+    });
+  })
+  // add listeners for each input
+
+  // set initial button state
+  toggleButtonState(buttonElement, inputList);
+}
+
+const enableValidation = ({ formSelector, ...restConfig }) => {
+  // const { formSelector, ...restConfig } = config;
+  // find all forms
+  const formList = Array.from(document.querySelectorAll(formSelector));
+
+  // set event lesteners each form
+  formList.forEach((formElement) => {
+    setEventListeners(formElement, restConfig);
+  })
 };
